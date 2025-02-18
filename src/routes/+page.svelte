302857b9af7fb2c40c96595ba439ca7aa72a7aab
Jorge <jmunoz241995@gmail.com>
2025-02-17 22:03:14 -0800

    header with image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
   import TextInput from "$lib/TextInput.svelte";
 </script>
 
-<div class="flex flex-col items-center">
-  <h1 class="text-4xl font-bold my-10">GitHub README Generator</h1>
+<div class="flex flex-col">
+  <div class="border-b border-gray-400 mb-2 flex items-center">
+    <div class="w-14 h-14">
+      <img src="/the-github-scrolls.png" alt="GitHub Scrolls">
+    </div>
+    <h1 class="text-2xl font-bold">GitHub README Generator</h1>
+  </div>
   <TextInput />
 </div>
 
