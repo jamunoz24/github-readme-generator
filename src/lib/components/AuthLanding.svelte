e44c76004524a8573d869294b2daa9bc4dd03ae8
Jorge <jmunoz241995@gmail.com>
2025-02-18 19:31:07 -0800

    feat: add welcome message to AuthLanding page

--- src/lib/components/AuthLanding.svelte
+++ src/lib/components/AuthLanding.svelte
@@ -12,12 +12,29 @@
 <!-- Remount to show transition animation -->
 {#if showTransition}
   <div class="flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-6xl "
-      in:fly={{ y: -30, opacity:0, duration: 2000, delay: 300 }}>
+    <h1 class="text-7xl mb-2"
+      in:fly={{ y: -60, opacity:0, duration: 3000, delay: 500 }}>
       Welcome
     </h1>
-    <div in:fade={{ duration: 1500, delay: 800}}>
-      <p>lalala</p>
+
+    <div class="flex flex-col items-center">
+      <div class="flex items-center my-2 space-x-2"
+      in:fade={{ duration: 2400, delay: 1800}}>
+        <div class="w-18 h-18">
+          <img src="/the-github-scrolls.png" alt="GitHub Scrolls">
+        </div>
+        <h4 class="text-4xl">The GitHub Scrolls</h4>
+        <div class="w-18 h-18">
+          <img src="/the-github-scrolls.png" alt="GitHub Scrolls">
+        </div>
+      </div>
+
+      <div class="w-120 text-center space-y-2 text-gray-400"
+      in:fade={{ duration: 2800, delay: 3000}}>
+        <p>The GitHub Scrolls is a readme generator. Whether it is for a repository or your GitHub profile, this application uses ChatGPT to generate you an appealing readme file in md format.</p>
+        <p>This application requires a GitHub authenticated sign-in, as well as an OpenAI API secret key to start generating.</p>
+      </div>
     </div>
+
   </div>
 {/if}
